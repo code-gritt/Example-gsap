858a1ce7e887027271744891708e62712e664acd
Gokul <153720121+code-gritt@users.noreply.github.com>
2025-04-11 15:58:02 +0530

    Update page.tsx

--- my-app/app/variation/two/page.tsx
+++ my-app/app/variation/two/page.tsx
@@ -8,48 +8,11 @@ import { SmoothScroll } from "../../../components/SmoothScroll";
 import React from "react";
 
 const prompt = Prompt({
-  weight: "600",
+  weight: "800",
   subsets: ["latin"],
 });
 
 export default function Home() {
-  // useEffect(() => {
-  //   gsap.registerPlugin(ScrollTrigger);
-
-  //   const tl = gsap.timeline({
-  //     scrollTrigger: {
-  //       trigger: ".hero",
-  //       start: "top top",
-  //       end: "+=1200%",
-  //       scrub: 0,
-  //       pin: true,
-  //       markers: false,
-  //     },
-  //   });
-
-  //   tl.fromTo(
-  //     ".flat-bottom-circle",
-  //     { scale: 0, y: "100%" },
-  //     {
-  //       scale: 1,
-  //       y: "-50%",
-  //       ease: "power1.inOut",
-  //       duration: 3,
-  //     }
-  //   )
-  //     .to(
-  //       "h1",
-  //       {
-  //         xPercent: -100,
-  //         color: "white",
-  //         ease: "power1.inOut",
-  //         duration: 1.2,
-  //       },
-  //       0.5
-  //     )
-  //     .set("body", { backgroundColor: "#113059" }, 1.2);
-  // }, []);
-
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
 
@@ -57,7 +20,7 @@ export default function Home() {
       scrollTrigger: {
         trigger: ".hero",
         start: "top top",
-        end: "+=1200%",
+        end: "+=1600%",
         scrub: true,
         pin: true,
         markers: false,
@@ -69,9 +32,9 @@ export default function Home() {
       { scale: 0, y: "100%" },
       {
         scale: 1,
-        y: "-50%",
-        ease: "power1.inOut",
-        duration: 3,
+        y: "-40%",
+        ease: "none",
+        duration: 2,
       }
     )
       .to(
@@ -79,38 +42,51 @@ export default function Home() {
         {
           x: "-100vw",
           opacity: 0,
-          ease: "power2.out",
+          ease: "none",
+          duration: 2.5,
         },
-        0.3
+        0
       )
       .to(
         ".and-foodies",
         {
           x: "100vw",
           opacity: 0,
-          ease: "power2.out",
+          ease: "none",
+          duration: 2.5,
         },
-        0.3
+        0
       )
       .set("body", { backgroundColor: "#113059" }, 0.8);
   }, []);
 
   return (
     <SmoothScroll>
+      <svg style={{ position: "absolute", width: 0, height: 0 }}>
+        <defs>
+          <filter id="squiggly">
+            <feTurbulence
+              baseFrequency="0.02"
+              numOctaves="1"
+              result="noise"
+              seed="10"
+            />
+            <feDisplacementMap in="SourceGraphic" in2="noise" scale="10" />
+          </filter>
+        </defs>
+      </svg>
       <div className="fixed-bg"></div>
       <main className="relative min-h-screen overflow-x-hidden bg-transparent text-white">
         <div className="full hero h-screen w-full overflow-hidden">
-          <div className="z-0 flat-bottom-circle absolute left-1/2 top-1/2 h-[142vmax] w-[200vmax] -translate-x-1/2 -translate-y-1/2 scale-0 bg-[#ffc100]">
-            {/* This div creates the flat-bottom circle effect */}
-          </div>
-          <div className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center space-y-4 text-[#113059]">
+          <div className="z-20 flat-bottom-circle absolute left-1/2 top-1/2 h-[142vmax] w-[200vmax] -translate-x-1/2 -translate-y-1/2 scale-0 bg-[#ffc100]"></div>
+          <div className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center space-y-0 text-[#113059]">
             <h2
-              className={`${prompt.className} text-[6vmin] font-bold for-families`}
+              className={`${prompt.className} text-[23vmin] leading-[0.9] font-bold for-families squiggly-text`}
             >
               For Families
             </h2>
             <h2
-              className={`${prompt.className} text-[6vmin] font-bold and-foodies`}
+              className={`${prompt.className} text-[23vmin] leading-[0.9] font-bold and-foodies squiggly-text`}
             >
               And Foodies
             </h2>
